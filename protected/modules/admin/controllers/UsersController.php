fdf62090a98d7dfd6abbc404acba1f89fecfc613
Serhii Hlushko <hlushko@clipkit.de>
2012-10-17 10:00:17 +0200

    Added access rules for user management

--- protected/modules/admin/controllers/UsersController.php
+++ protected/modules/admin/controllers/UsersController.php
@@ -8,5 +8,29 @@
  */
 class UsersController extends AdminAbstractController 
 {
-    
+    /**
+     * {@inheritdoc}
+     */
+    public function accessRules()
+    {
+        return array(
+            array('allow', // allow authenticated users to perform 'view' actions
+                'actions' => array('admin', 'create', 'validate'),
+                'users' => array('@'),
+            ),
+            array('allow', // allow authenticated users to perform 'view' actions
+                'actions' => array('edit', 'delete'),
+                'users' => array('@'),
+                'expression' => '$user->id == 1',
+            ),
+            array('allow', // allow authenticated users to perform 'view' actions
+                'actions' => array('edit', 'delete'),
+                'users' => array('@'),
+                'expression' => '$user->id == Yii::app()->request->getQuery(\'id\')',
+            ),
+            array('deny', // deny all users
+                'users' => array('*'),
+            ),
+        );
+    }
 }
